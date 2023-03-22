2a6e1eea74376daa89c04dd59cb39befec159de4
Richard <zfanli@outlook.com>
2023-03-23 00:42:25 +0800

    chore: 更新部分数据库设置

--- packages/server/src/common/database.module.ts
+++ packages/server/src/common/database.module.ts
@@ -1,6 +1,7 @@
 import { Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { SnakeNamingStrategy } from './snake-naming.strategy'
 
 // 数据库实体需要在这里注册
 const logger = new Logger('DatabaseModule')
@@ -9,7 +10,11 @@ export const DatabaseModule = TypeOrmModule.forRootAsync({
   useFactory: (configService: ConfigService) => {
     const config = configService.get('db')
     logger.debug('current database config:', config)
-    return { ...config }
+    return {
+      ...config,
+      namingStrategy: new SnakeNamingStrategy(),
+      autoLoadEntities: true,
+    }
   },
   inject: [ConfigService],
 })
